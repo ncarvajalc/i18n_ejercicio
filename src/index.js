80fb7e3c3e548c7ec18cee3f05f2bc6d9f42950f
ncarvajalc <n.carvajalc@uniandes.edu.co>
2022-05-13 13:54:45 -0500

    Ejercicio

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import JobsList from "./components/jobslist";
+import { IntlProvider } from "react-intl";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const lan = navigator.language || navigator.userLanguage;
+
+const getLocale = () => {
+  return lan === "en" ? localeEnMessages : localeEsMessages;
+};
+ReactDOM.render(
+  <IntlProvider locale={lan} messages={getLocale()}>
+    <JobsList />
+  </IntlProvider>,
+  document.getElementById("root")
+);
